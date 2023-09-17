a23c6289453c57a373cac31dbe6c92464b712a5c
NastiaDamian <nastuhta2005@gmail.com>
2023-09-17 20:50:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string my_name = "Nastia";
+Console.WriteLine(my_name);
+
+
+int value1 = Convert.ToInt32(Console.ReadLine());
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+//Функція додавання
+Console.WriteLine($" {value1} + {value2} = {value1 + value2} ");
+int result = value1 + value2;
+
+//Функція віднімання 
+Console.WriteLine($" {value1} - {value2} = {value1 - value2} ");
+int result_2 = value1 - value2;
+
+//Функція множення 
+Console.WriteLine($" {value1} * {value2} = {value1 * value2} ");
+int result_3 = value1 * value2;
+
+//Функція ділення 
+Console.WriteLine($" {value1} / {value2} = {value1 / value2} ");
+int result_4 = value1 / value2;
+
+//Остача від цілочисельного ділення
+Console.WriteLine($" {value1} % {value2} = {value1 % value2} ");
+int result_5 = value1 % value2;
